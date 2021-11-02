92ca913750b97a3550b47e6682a96851a739403b
Dmitry <dimassdf5@fmail.com>
2021-11-02 14:33:25 +0300

    End using of RecyclerView and CardView

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -8,6 +10,10 @@ import android.widget.ArrayAdapter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView; // Связывает с JavaCode
+    private RecyclerView.Adapter adapter; // Мост между с RecyclerView и данными (управляет неполным заполнением)
+    private RecyclerView.LayoutManager layoutManager; // Управляет расположением элементов
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
                 "Neutral", "Life is life"));
         recyclerViewItems.add(new RecyclerViewItem(R.drawable.ic_baseline_sentiment_very_dissatisfied_24,
                 "Sad", "Life is sad"));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true); // Для фиксированного места, для быстроты работы приложения
+        adapter = new RecyclerViewAdapter(recyclerViewItems);
+        layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
